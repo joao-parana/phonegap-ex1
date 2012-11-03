aa3f47f06c63eb0e3a283ce7ef1995aa0b09fff3
Joao Antonio Ferreira <joao.parana@gmail.com>
2012-11-02 22:23:26 -0200

    exercice 1

---
+++ app.js
@@ -0,0 +1,75 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express')
+  , routes = require('./routes')
+  , User = require('./models/User.js')
+  , http = require('http')
+  , path = require('path')
+  , fs = require('fs');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 80);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/sayhello', function(req, res){
+  fs.readFile('./form.html', function(err, contents) {
+    if (err) {
+      res.writeHead(500);
+      res.end();
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(contents, 'utf-8');
+    }
+  });  
+});
+
+app.get('/form', function(req, res){
+  fs.readFile('./form.html', function(err, contents) {
+    if (err) {
+      res.writeHead(500);
+      res.end();
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(contents, 'utf-8');
+    }
+  });  
+});
+
+app.post('/sayhello', function(req, res){
+  res.send('Hello ' + req.body.name);
+});
+
+app.get('/users', User.list);
+
+app.post('/signup', function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  User.addUser(username, password, function(err, user) {
+	  console.log('Executando callback do metodo User.addUser');
+	  
+	  if (err) throw err;
+	  
+	  console.log(user);
+	  res.redirect('/form');
+  });
+});
+	  
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
---
+++ models/User.js
@@ -0,0 +1,28 @@
+var db = require('../lib/db');
+
+var UserSchema = new db.Schema({
+    username : { type: String, unique: true }
+  , password : String
+});
+
+var MyUser = db.mongoose.model('User', UserSchema);
+
+// Exports
+module.exports.addUser = addUser;
+// Add user to database
+function addUser(username, password, callback) {
+  var instance = new MyUser();
+  console.log('username=' + username + ', password=' + password + ', na execucao do addUser');
+  console.log('callback=' + callback);
+  instance.username = username;
+  instance.password = password;
+  instance.save(function (err) {
+    if (err) {
+      callback(err);
+	}
+	else {
+      callback(null, instance);
+    }
+  }); 
+}
+
